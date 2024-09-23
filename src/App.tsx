8fef9e6e4904c745963d5c6b2af9dc2c9a447f16
fabqw <ffquispe8@umsa.comgit remote -vgit config --global user.name fabqwgit config --global user.email ffquispe8@umsa.com>
2024-09-23 16:17:05 -0400

    Commit Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import './App.css'
 import Register from './Screens/Register.tsx';
 import Login from './Screens/Login.tsx';
 import { Route, Routes } from 'react-router-dom';
-
+import Navbar from '../src/Components/Navbar.tsx'
 function App() {
 
   return (
-    <div>
+    <>
+    <Navbar/>
       <Routes>
         <Route path='/Login' element={<Login/>}/>
         <Route path='/Register' element={<Register/>}/>
       </Routes>
-    </div>
+    </>
   )
 }
 
