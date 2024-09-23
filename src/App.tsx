2f0e05a621381992e18308f24502e6a76ab09ea1
Romer <romer141016@gmail.com>
2024-09-23 18:29:01 -0400

    MaquetacionAdminhome

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.css'
 import Register from './Screens/Register.tsx';
 import Login from './Screens/Login.tsx';
 import Navbar from '../src/Components/Navbar.tsx'
+import Home from './Screens/Admin/home.tsx';
+
+
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -14,6 +17,7 @@ function App() {
       <Routes>
         <Route path='/Login' element={<Login/>}/>
         <Route path='/Register' element={<Register/>}/>
+        <Route path='/Admin/home' element={<Home/>}/>
       </Routes>
     </>
   )
